d2ac32a5803d96716434f2cf858941e1b1b97650
Quang Hoang <dangquangvip64@gmail.com>
2023-11-16 17:00:52 +0700

    fix product detail layout

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -9,29 +9,27 @@ const ProductDetail = () => {
   }, []);
   return (
     <div>
-      <div className="max-w-screen-xl mx-auto flex my-10 ">
-        <div>
+      <div className="max-w-screen-xl mx-auto flex my-10 gap-10">
+        <div className="w-2/5">
           <img
-            className="w-full px-10 py-10 border-[5px]"
+            className="h-[550px] px-10 py-10 object-cover "
             src={details.image}
             alt="ProductImage"
           ></img>
         </div>
-        <div className="pl-10 flex flex-col justify-center">
-          <h2 className="text-4xl font-bold font-titleFont">
-            {details.title}
-          </h2>
-          <div className="my-5">
-            <p className="text-2xl font-semibold font-title">
-              ${details.price}
-            </p>
+
+        <div className="w-3/5">
+          <div>
+            <h2 className="text-4xl font-semibold font-titleFont">{details.title}</h2>
+            <div className="py-5">
+              <p className="text-2xl font-bold font-titleFont">${details.price}</p>
+            </div>
           </div>
           <h2 className="py-5 text-2xl">Details</h2>
-          <p className="text-sm font-titleFont text-gray-500">
-            {details.description}
-          </p>
+          <p className="text-sm font-titleFont">{details.description}</p>
+          
           <div className="my-10 flex gap-10">
-            <div className="p-2 w-48 flex items-center justify-between border font-titleFont text-gray-600">
+            <div className="p-2 w-40 flex items-center justify-between border font-titleFont text-gray-600">
                 <p className="text-sm">Quantity</p>
                 <div className="flex items-center gap-4">
                     <button>-</button>
@@ -39,7 +37,7 @@ const ProductDetail = () => {
                     <button>+</button>
                 </div>
             </div>
-            <button className="bg-red-500 text-white py-3 px-6 active:bg-red-500 hover:bg-red-400">Add to cart</button>
+            <button className="bg-red-500 text-white py-3 px-6 active:bg-red-500 hover:bg-red-400 rounded-full">Add to cart</button>
           </div>
         </div>
       </div>
