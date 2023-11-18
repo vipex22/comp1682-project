42d84852b825a3abf158e75a7f6e3d26802ecf55
Quang Hoang <dangquangvip64@gmail.com>
2023-11-18 16:45:34 +0700

    Save product to local storage by using redux-persist and decorate some aspect in cart page

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,8 +1,10 @@
-import Logo from "./LogoTrans.png"
-import Cart from "./shopping-cart.png"
-import UserAvatar from "./useravatar.png"
-import Banner1 from "./Banner1.jpg"
-import Banner2 from "./Banner2.jpg"
-import Banner3 from "./Banner3.jpg"
-import GWlogo from "./Official_logo_of_Greenwich_Vietnam.png"
-export {Logo, Cart, UserAvatar, Banner1, Banner2, Banner3, GWlogo};
+import Logo from "./LogoTrans.png";
+import Cart from "./shopping-cart.png";
+import UserAvatar from "./useravatar.png";
+import Banner1 from "./Banner1.jpg";
+import Banner2 from "./Banner2.jpg";
+import Banner3 from "./Banner3.jpg";
+import GWlogo from "./Official_logo_of_Greenwich_Vietnam.png";
+import Cart2 from "./trolley.png";
+
+export { Logo, Cart, UserAvatar, Banner1, Banner2, Banner3, GWlogo, Cart2 };
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -3,45 +3,65 @@ import { useState, useEffect } from "react";
 import CartItem from "../components/CartItem";
 import { ToastContainer } from "react-toastify";
 import { useSelector } from "react-redux";
-
+import { Link } from "react-router-dom";
+import {Cart2} from "../assets/index";
 const Cart = () => {
-  const productData = useSelector((state)=> state.Cart.productData)
+  const productData = useSelector((state) => state.Cart.productData);
+  console.log("product amount: ", productData.length);
   const [totalPrice, setTotalPrice] = useState();
-  useEffect(()=>{
+  useEffect(() => {
     let total = 0;
-    productData.map((item)=>{
+    productData.map((item) => {
       total += item.productPrice * item.productQuantity;
-      return total
+      return total;
     });
-    setTotalPrice(total);
-  },[productData])
-  return (
-    <div className="max-w-screen-xl mx-auto py-20 flex">
-      <CartItem />
-      <div className="w-1/3 py-6 px-4">
-        <div className="flex flex-col gap-6 border-b-[1px] border-b-gray-400 pb-6">
-          <h2 className="text-2xl font-bold font-titleFont">Totals</h2>
-          <p className="flex items-start gap-8 text-base">
-            Address
-            <span className="text-lg font-bold font-titleFont">
-              Da Nang, Hai Chau, Ong Ich Khiem street
-            </span>
+    setTotalPrice(total.toFixed(2));
+  }, [productData]);
+
+  const renderProduct = () => {
+    if (productData.length === 0) {
+      return (
+        <div className="max-w-screen-xl mx-auto py-20 items-center flex flex-col justify-center">
+          <img className="w-28 mt-10" src={Cart2} alt="cartImg"></img>
+          <p className="text-2xl font-semibold font-titleFont text-gray-300 mt-10">
+            Your shopping cart is empty
           </p>
+          <Link
+            className="w-60 text-base text-center font-titleFont bg-red-500 text-white py-3 px-3 mt-7 mb-10 active:bg-red-500 hover:bg-red-400"
+            to={"/"}
+          >
+            Shop Now
+          </Link>
         </div>
-        <p className="font-semibold flex justify-between mt-6 font-titleFont">
-          Total payment <span className="text-xl font-bold">$ {totalPrice}</span>
-        </p>
-        <button className="bg-red-500 text-white w-full py-3 mt-5 active:bg-red-500 hover:bg-red-400">
-          Purchase
-        </button>
+      );
+    }
+    return (
+      <div className="max-w-screen-xl mx-auto py-20 flex">
+        <CartItem />
+        <div className="w-1/3 py-6 px-4">
+          <div className="flex flex-col gap-6 border-b-[1px] border-b-gray-400 pb-6">
+            <h2 className="text-2xl font-bold font-titleFont">Totals</h2>
+            <p className="flex items-start gap-8 text-base">
+              Address
+              <span className="text-lg font-bold font-titleFont">
+                Da Nang, Hai Chau, Ong Ich Khiem street
+              </span>
+            </p>
+          </div>
+          <p className="font-semibold flex justify-between mt-6 font-titleFont">
+            Total payment{" "}
+            <span className="text-xl font-bold">$ {totalPrice}</span>
+          </p>
+          <button className="bg-red-500 text-white w-full py-3 mt-5 active:bg-red-500 hover:bg-red-400">
+            Purchase
+          </button>
+        </div>
+        <ToastContainer position="top-right" autoClose={5000} theme="light" />
       </div>
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        theme="light"
-      />
-    </div>
-  );
+    );
+  };
+
+  return <div>{renderProduct()}</div>;
 };
 
 export default Cart;
