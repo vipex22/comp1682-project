3b9deef88d1d8a3085c1c6fa3a616123c7b9c833
Quang Hoang <dangquangvip64@gmail.com>
2023-11-25 21:28:31 +0700

    Add edit product, fix fetch to be realtime, add delete and show detail product too

---
+++ src/components/EditProduct.js
@@ -0,0 +1,179 @@
+import React, { useState, useEffect } from "react";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { doc, updateDoc } from "firebase/firestore";
+import { storage, firestore } from "../firebase";
+import { toast } from "react-toastify";
+
+const EditProduct = ({ product, onClose }) => {
+  const [title, setTitle] = useState(product ? product.title || "" : "");
+  const [price, setPrice] = useState(product ? product.price || "" : "");
+  const [category, setCategory] = useState(product ? product.category || "" : "");
+  const [description, setDescription] = useState(product ? product.description || "" : "");
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    if (product) {
+      console.log("Setting initial values:", product);
+      setTitle(product.title || "");
+      setPrice(product.price || "");
+      setCategory(product.category || "");
+      setDescription(product.description || "");
+    }
+  }, [product]);
+  
+  const handleEditProduct = async () => {
+    try {
+      if (title && price && category && description) {
+        let imageUrl = product.image;
+
+        if (image) {
+          const storageRef = ref(storage, `images/${image.name}`);
+          await uploadBytes(storageRef, image);
+          imageUrl = await getDownloadURL(storageRef);
+        }
+
+        const productDocRef = doc(firestore, "products", product.id);
+        await updateDoc(productDocRef, {
+          title,
+          price,
+          category,
+          description,
+          image: imageUrl,
+        });
+
+        toast.success("Product updated successfully");
+        onClose();
+      } else {
+        alert("Please fill in all required fields");
+      }
+    } catch (error) {
+      console.error("Error updating product:", error);
+    }
+  };
+
+  const handleImageChange = (e) => {
+    const selectedImage = e.target.files[0];
+    setImage(selectedImage);
+  };
+
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-10 backdrop-blur-sm flex justify-center items-center">
+      <div className="bg-white w-80 p-4 mt-12 rounded-lg">
+        <div className="flex justify-between">
+          <h2 className="text-xl font-semibold mb-4">Edit Product</h2>
+          <button
+            onClick={onClose}
+            className="bg-gray-300 rounded-full w-6 h-6 justify-center items-center"
+          >
+            X
+          </button>
+        </div>
+        <form>
+          <div className="mb-4">
+            <label
+              htmlFor="title"
+              className="block text-sm font-medium text-gray-600"
+            >
+              Product Title
+            </label>
+            <input
+              type="text"
+              id="title"
+              name="title"
+              className="mt-1 p-2 border border-gray-300 rounded-md w-full"
+              onChange={(e) => setTitle(e.target.value)}
+              value={title}
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="price"
+              className="block text-sm font-medium text-gray-600"
+            >
+              Price
+            </label>
+            <input
+              type="text"
+              id="price"
+              name="price"
+              className="mt-1 p-2 border border-gray-300 rounded-md w-full"
+              onChange={(e) => setPrice(e.target.value)}
+              value={price}
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="category"
+              className="block text-sm font-medium text-gray-600"
+            >
+              Category
+            </label>
+            <select
+              id="category"
+              name="category"
+              className="mt-1 p-2 border border-gray-300 rounded-md w-full"
+              onChange={(e) => setCategory(e.target.value)}
+              value={category}
+              required
+            >
+              <option value="" disabled>
+                Select a category
+              </option>
+              <option value="Shirt">Shirt</option>
+              <option value="Pant">Pant</option>
+              <option value="Hat">Hat</option>
+              <option value="Shoes">Shoes</option>
+              <option value="Hoodie">Hoodie</option>
+              <option value="Bag">Bag</option>
+            </select>
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="description"
+              className="block text-sm font-medium text-gray-600"
+            >
+              Description:
+            </label>
+            <textarea
+              id="description"
+              name="description"
+              rows="3"
+              className="mt-1 p-2 border border-gray-300 rounded-md w-full"
+              onChange={(e) => setDescription(e.target.value)}
+              value={description}
+              required
+            ></textarea>
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="image"
+              className="block text-sm font-medium text-gray-600"
+            >
+              Image
+            </label>
+            <input
+              type="file"
+              id="image"
+              name="image"
+              className="mt-1 p-2 border border-gray-300 rounded-md w-full"
+              onChange={handleImageChange}
+              accept="image/*"
+              required
+            />
+          </div>
+          <button
+            type="button"
+            onClick={handleEditProduct}
+            className="bg-blue-500 text-white py-2 px-3 active:bg-blue-500 hover:bg-blue-400 mx-auto rounded flex justify-center items-center"
+          >
+            Edit Product
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default EditProduct;
--- src/pages/ManageProducts.js
+++ src/pages/ManageProducts.js
@@ -2,16 +2,11 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { auth, firestore, storage } from "../firebase";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
-import {
-  doc,
-  getDoc,
-  getDocs,
-  collection,
-  addDoc,
-  updateDoc,
-} from "firebase/firestore";
+import { doc, getDoc, deleteDoc, collection, addDoc } from "firebase/firestore";
 import { AdminIcon } from "../assets";
 import { toast, ToastContainer } from "react-toastify";
+import EditProduct from "../components/EditProduct";
+import { onSnapshot } from "firebase/firestore";
 
 const ManageProducts = () => {
   const [user, setUser] = useState(null);
@@ -60,21 +55,18 @@ const ManageProducts = () => {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    const fetchProducts = async () => {
+    const fetchProducts = () => {
       const productsRef = collection(firestore, "products");
 
-      try {
-        const productsSnapshot = await getDocs(productsRef);
-        const productsData = [];
-
-        productsSnapshot.forEach((doc) => {
-          productsData.push({ id: doc.id, ...doc.data() });
-        });
-
+      const unsubscribe = onSnapshot(productsRef, (snapshot) => {
+        const productsData = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
         setProducts(productsData);
-      } catch (error) {
-        console.error("Error fetching products:", error);
-      }
+      });
+
+      return unsubscribe;
     };
 
     fetchProducts();
@@ -118,6 +110,7 @@ const ManageProducts = () => {
       const selectedImage = e.target.files[0];
       setImage(selectedImage);
     };
+
     return (
       <div className="fixed inset-0 bg-black bg-opacity-10 backdrop-blur-sm flex justify-center items-center">
         <div className="bg-white w-80 p-4 mt-12 rounded-lg ">
@@ -218,7 +211,7 @@ const ManageProducts = () => {
                 name="image"
                 className="mt-1 p-2 border border-gray-300 rounded-md w-full"
                 onChange={handleImageChange}
-                accept="image/*" 
+                accept="image/*"
                 required
               />
             </div>
@@ -235,6 +228,42 @@ const ManageProducts = () => {
     );
   };
 
+  //Edit product
+  const [isEditModalOpen, setEditModalOpen] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState(null);
+
+  const handleEditProduct = (product) => {
+    setSelectedProduct(product);
+    setEditModalOpen(true);
+  };
+  //
+
+  //Delete product
+  const handleDeleteProduct = async (productId) => {
+    const confirmDelete = window.confirm(
+      "Are you sure you want to delete this product?"
+    );
+
+    if (confirmDelete) {
+      try {
+        const productRef = doc(firestore, "products", productId);
+        await deleteDoc(productRef);
+        toast.success("Product deleted successfully");
+      } catch (error) {
+        console.error("Error deleting product:", error);
+      }
+    }
+  };
+
+  //showdetail
+  const handleProductDetail = (product) => {
+    const productId = product.id;
+    navigate(`/product/${productId}`, {
+      state: {
+        items: product,
+      },
+    });
+  };
   return (
     <div className="min-h-[650px]">
       {isAdmin && (
@@ -251,66 +280,80 @@ const ManageProducts = () => {
             </button>
           </div>
           <div className="max-h-96 my-10 min-h-[650px] overflow-y-auto border-2 border-gray-400">
-          {products.length === 0 ? (
+            {products.length === 0 ? (
               <div className="max-w-screen-xl mx-auto py-20 items-center flex flex-col justify-center">
-              <img className="w-28 mt-36" src={AdminIcon} alt="cartImg"></img>
-              <p className="text-2xl font-semibold font-titleFont text-gray-300 mt-10">
-                No order has been made!
-              </p>
-              
-            </div>
+                <img className="w-28 mt-36" src={AdminIcon} alt="cartImg"></img>
+                <p className="text-2xl font-semibold font-titleFont text-gray-300 mt-10">
+                  No order has been made!
+                </p>
+              </div>
             ) : (
-            <table className="table-auto w-full font-titleFont border-2">
-              <thead>
-                <tr>
-                  <th className="border px-4 py-2 bg-gray-200">Image</th>
-                  <th className="border px-4 py-2 bg-gray-200">Title</th>
-                  <th className="border px-4 py-2 bg-gray-200">Price</th>
-                  <th className="border px-4 py-2 bg-gray-200">Category</th>
-                  <th className="border px-4 py-2 bg-gray-200">Description</th>
-                  <th className="border px-4 py-2 bg-gray-200">Operation</th>
-                </tr>
-              </thead>
-              <tbody>
-                {products.map((product) => (
-                  <tr key={product.id}>
-                    <td className="border px-4 py-2 w-40 text-center">
-                      <img
-                        className="px-2 py-2 object-cover "
-                        src={product.image}
-                        alt="ProductImage"
-                      ></img>
-                    </td>
-                    <td className="border px-4 py-2 w-40 text-center">
-                      {product.title}
-                    </td>
-                    <td className="border px-4 py-2 w-20 text-center">
-                      ${product.price}
-                    </td>
-                    <td className="border px-4 py-2 w-20 text-center">
-                      {product.category}
-                    </td>
-                    <td className="border px-4 py-2 w-80 text-center">
-                      {product.description}
-                    </td>
-                    <td className="border px-4 py-2 w-32 text-center">
-                      <button className="bg-blue-500 text-white py-2 px-3  active:bg-blue-500 hover:bg-blue-400 rounded mr-2">
-                        Edit
-                      </button>
-                      <button className="bg-red-500 text-white py-2 px-3  active:bg-red-500 hover:bg-red-400 rounded">
-                        Delete
-                      </button>
-                    </td>
+              <table className="table-auto w-full font-titleFont border-2">
+                <thead>
+                  <tr>
+                    <th className="border px-4 py-2 bg-gray-200">Image</th>
+                    <th className="border px-4 py-2 bg-gray-200">Title</th>
+                    <th className="border px-4 py-2 bg-gray-200">Price</th>
+                    <th className="border px-4 py-2 bg-gray-200">Category</th>
+                    <th className="border px-4 py-2 bg-gray-200">
+                      Description
+                    </th>
+                    <th className="border px-4 py-2 bg-gray-200">Operation</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  {products.map((product) => (
+                    <tr key={product.id}>
+                      <td className="border px-4 py-2 w-40 text-center">
+                        <img
+                          onClick={() => handleProductDetail(product)}
+                          className="px-2 py-2 object-cover cursor-pointer"
+                          src={product.image}
+                          alt="ProductImage"
+                        />
+                      </td>
+                      <td className="border px-4 py-2 w-40 text-center">
+                        {product.title}
+                      </td>
+                      <td className="border px-4 py-2 w-20 text-center">
+                        ${product.price}
+                      </td>
+                      <td className="border px-4 py-2 w-20 text-center">
+                        {product.category}
+                      </td>
+                      <td className="border px-4 py-2 w-80 text-center">
+                        {product.description}
+                      </td>
+                      <td className="border px-4 py-2 w-32 text-center">
+                        <button
+                          onClick={() => handleEditProduct(product)}
+                          className="bg-blue-500 text-white py-2 px-3  active:bg-blue-500 hover:bg-blue-400 rounded mr-2"
+                        >
+                          Edit
+                        </button>
+                        <button
+                          onClick={() => handleDeleteProduct(product.id)}
+                          className="bg-red-500 text-white py-2 px-3  active:bg-red-500 hover:bg-red-400 rounded"
+                        >
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
             )}
           </div>
         </div>
       )}
       {isCreateModalOpen && (
         <CreateProductModal onClose={() => setCreateModalOpen(false)} />
+      )}
+      {isEditModalOpen && (
+        <EditProduct
+          product={selectedProduct}
+          onClose={() => setEditModalOpen(false)}
+        />
       )}
       <ToastContainer position="top-right" autoClose={5000} theme="light" />
     </div>
